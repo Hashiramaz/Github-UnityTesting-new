18bad2d32c50cdfdf5f29df6a5a38054ac584d28
VinniHashirama <vinicius_freires@hotmail.com>
2018-07-25 11:42:36 -0300

    another commit?

---
+++ Assets/Scripts/ScriptTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest : MonoBehaviour {
+
+    private int gameobjectTest;
+    public int secondvariable;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void EnableComponentes()
+    {
+
+    }
+
+
+
+
+
+    void DisableEveryComponents()
+    {
+
+    }
+}
